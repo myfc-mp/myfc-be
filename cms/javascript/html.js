209a23c6a7c9ffd9eea96ec7f3f7f0bacbb2e969
lxb365 <36372674+lxb365@users.noreply.github.com>
2018-04-17 18:14:50 +0800

    更新css

--- cms/javascript/html.js
+++ cms/javascript/html.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	let px=Math.floor($("body").innerWidth()/50)-4;
+	$("html").css("font-size",px+"px");	
+
     $("#addHouse").click(function(){
       $("#subAddHouse").slideToggle("slow");
     });
@@ -6,4 +9,12 @@ $(document).ready(function(){
     $("#mngHouse").click(function(){
         $("#subMngHouse").slideToggle("slow");
       });
+    $("#shopMng").click(function(){
+    	console.log($("body").innerWidth()); 
+    });
   });
+
+$(window).resize(function(){
+	let px=Math.floor($("body").innerWidth()/50)-4;
+	$("html").css("font-size",px+"px");	
+});
