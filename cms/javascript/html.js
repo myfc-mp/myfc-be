6c125cd124511782a4be13ebb2a0dea81fc20cdd
lxb365 <36372674+lxb365@users.noreply.github.com>
2018-04-17 09:15:22 +0800

    更新cms

---
+++ cms/javascript/html.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#addHouse").click(function(){
+      $("#subAddHouse").slideToggle("slow");
+    });
+
+    $("#mngHouse").click(function(){
+        $("#subMngHouse").slideToggle("slow");
+      });
+  });
